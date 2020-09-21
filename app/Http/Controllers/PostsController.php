0600942239fd934bb45f6e16f1f0cb5cba7cb9e1
HasanBaykal <baykalhh@gmail.com>
2020-09-21 18:36:32 +0200

    Set up Eloq Model for Posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-          
             return view('post', [
-                'post' => $post
+                'post' => Post::where('slug', $slug)->firstOrFail()
             ]);
     }
 }
