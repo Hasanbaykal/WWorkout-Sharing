5a7ece035d275118f51badaebbc4eb33a587fed9
HasanBaykal <baykalhh@gmail.com>
2020-10-02 15:08:06 +0200

    Homepage Layout Complete

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,101 +4,86 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <div class="background-img" style="background-image: url('https://img4.goodfon.com/original/1920x1080/d/5a/bodybuilding-bodibilder-weight-training-muscles-bodybuilder.jpg')">;
-
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
-            html, body {
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+           
+        html, body {
+            font-family: 'Raleway', sans-serif;
+            height: 100%;
+            background: url('https://img4.goodfon.com/original/1920x1080/d/5a/bodybuilding-bodibilder-weight-training-muscles-bodybuilder.jpg') no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            background-size: cover;
+            -o-background-size: cover;
+            overflow-x: hidden;
+            -webkit-text-size-adjust: 100%;
+        }
 
-            .background-img {
-                width: 100%;
-                height: auto;
-                background-size: cover;
-                background-position: center center;
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-            }
+        .homepage-title {
+            color: rgba(255,255,255,0.4);
+            margin-bottom: 0px;
+            padding-bottom: 0px;
+            font-size: 425%;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .homepage-subtitle {
+            color: rgba(255,255,255,0.4);
+            margin-top: 0px;
+            padding-top: 0px;
+            font-size: 100%;
+            text-align: center;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .active {
+            display: block;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .menu {
+            position: fixed; 
+            width: 14%;
+            z-index: 1; 
+            top: 2%;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .menu-list1 {
+            position: fixed; 
+            width: 12%;
+            z-index: 1; 
+            top: 80%;
+        }
 
-            .title {
-                font-size: 50px;
-                text-align: center;
-            }
+        #menu-list {
+            background-color: rgba(140, 114, 114, 0.2);
+            border-radius: 15px;
+            font-size: 16px; 
+            padding: 0;
+            text-align: justify;
+        }
 
-            .active {
-      display: block;
-    }
-
-            .menu {
-    position: fixed; 
-    width: 14%;
-    z-index: 1; 
-    top: 2%;
-}
-
-  .menu-list1 {
-    position: fixed; 
-    width: 12%;
-    z-index: 1; 
-    top: 80%;
-  }
-
-#menu-list {
-    background-color: rgba(120, 180, 0, 0.8);
-    border-radius: 15px;
-    font-size: 16px; 
-    padding: 0;
-    text-align: justify;
-}
-
-#menu-list > li {
-    display: block;
-}
+        #menu-list > li {
+            display: block;
+        }
 
 
-#menu-list > li > a {
-    font-family: 'Raleway', sans-serif; 
-    padding: 10px 0 10px 30px; 
-    text-decoration: none; 
-    display: block;
-    color: #fff; 
-}
+        #menu-list > li > a {
+            font-family: 'Raleway', sans-serif; 
+            padding: 10px 0 10px 30px; 
+            text-decoration: none; 
+            display: block;
+            color: #fff; 
+        }
 
-#menu-list > .active > a,
-#menu-list > li > a:hover { 
-    background-color: rgba(255,255,255,0.25);
-    border-radius: 15px;
-}
+        #menu-list > .active > a,
+        #menu-list > li > a:hover { 
+            background-color: rgba(255,255,255,0.25);
+            border-radius: 15px;
+        }
 
         </style>
+
     </head>
     <body>
         @yield ('header')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,40 @@
 @extends ('layout')
 
 @section ('content')
-<div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+<div class="menu visible-md visible-lg">
+    <div class="top_bg">
+        <div class="wrap">
+            <div class="container">
+                <div class="homepage-title">HBKFit</div>
+                <div class="homepage-subtitle">Explore the Experience</div>
+                <div class="clear"></div>
+        </div>  
+    </div>
 </div>
-
-            <div class="menu visible-md visible-lg">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                        <ul id="menu-list">
-                            <li class="active"><a href="http://127.0.0.1:8000/forum">Forum</a></li>
-                            <li class=""><a href="http://127.0.0.1:8000/contact">Contact</a></li>
-                        </ul>
-
-       <!--     @if (Route::has('login'))
-                <ul id="menu-list" class="menu-list1">
-            @auth
-                <li><a href="{{ url('/home') }}">Home</a></li>
-            @else
-                <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span> Login</a></li>
-            @if (Route::has('register'))
-                <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-log-in"></span> Register</a></li>
-            @endif
-            @endauth
-                </ul>
-            @endif
-
-            -->
+      
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul id="menu-list">
+        <li class="active"><a href="http://127.0.0.1:8000/">Home</a></li>
+        <li class=""><a href="http://127.0.0.1:8000/forum">Forum</a></li>
+        <li class=""><a href="http://127.0.0.1:8000/contact">Contact</a></li>
+      </ul>
+      @if (Route::has('login'))
+      <ul id="menu-list">
+      @auth
+      <li><a href="{{ url('/home') }}">Home</a></li>
+      @else
+        <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+        @if (Route::has('register'))
+        <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-log-in"></span> Register</a></li>
+        @endif
+      @endauth
+      </ul>
+      <ul id="menu-list" class="menu-list1">
+        <li><a href="http://127.0.0.1:8000/admin">Admin</a></li>
+      </ul>
+      @endif
+    </div>
+    </div>
+           
 @endsection
