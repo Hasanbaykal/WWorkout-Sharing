2f0895d7777b1a792d9a75c24c51cedb833b5130
HasanBaykal <baykalhh@gmail.com>
2020-10-03 12:11:22 +0200

    Created the Group Route for Admin
    Login, Logout & Register

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,16 @@ Route::get('/posts/{post}', 'PostsController@show');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function () {
+    // Login routes
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
+    // Logout route
+    Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+    // Register route
+    Route::get('/register', 'Auth\AdminRegisterController@showRegistrationForm')->name('admin.register');
+    Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register.submit');
+});
